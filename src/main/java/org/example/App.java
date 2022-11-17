b52229ebd88c33e1607c24da6f67c1d19a4f4b42
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-17 20:47:50 +0530

    MODIFIED: Code to rename Constant

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // Constants
-        int IS_ATTENDANCE = 1;
+        int IS_FULL_TIME = 1;
         int attendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
-        if (attendance == IS_ATTENDANCE) {
+        if (attendance == IS_FULL_TIME) {
             System.out.println("Employee is Present");
         } else {
             System.out.println("Employee is Absent");
