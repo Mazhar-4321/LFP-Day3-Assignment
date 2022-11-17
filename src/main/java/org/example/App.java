24182bdf8abc4772e81b836374456e7b7f0ef3e2
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-17 20:42:01 +0530

    ADDED: Code to find employee attendance

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,13 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        // Constants
+        int IS_ATTENDANCE = 1;
+        int attendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
+        if (attendance == IS_ATTENDANCE) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
