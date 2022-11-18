8e6c22d6e0c7dc3dfd475a70ded9db8488738a8f
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 06:57:07 +0530

    ADDED: Code to check Employee Nature and Calculate Part Time Wage calculator

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,16 +4,26 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // Constants
-        int IS_FULL_TIME = 1;
+        int IS_PRESENT = 1;
         int WAGE_PER_HOUR = 20;
         int WORKING_HOURS_IN_A_DAY = 8;
-        int attendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
-        if (attendance == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
+        int PART_TIME_WORKING_HOURS = 8;
+        // Variables
+        int fullTimeattendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
+        int partTimeAttendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
+        // Functionality
+        if (fullTimeattendance == IS_PRESENT) {
+            System.out.println("Nature Of Employee: Full Time , Status: Present");
             System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, WORKING_HOURS_IN_A_DAY);
             System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY));
         } else {
-            System.out.println("Employee is Absent");
+            if (partTimeAttendance == IS_PRESENT) {
+                System.out.println("Nature Of Employee: Part Time , Status: Present");
+                System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, PART_TIME_WORKING_HOURS);
+                System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS));
+            } else {
+                System.out.println("Employee is Absent");
+            }
         }
     }
 }
