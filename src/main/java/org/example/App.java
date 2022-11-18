0763e2384f901e711573feea50939a0f53546c36
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 07:38:49 +0530

    ADDED: Code to calculate monthly wages of an Employee

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App {
         int WAGE_PER_HOUR = 20;
         int WORKING_HOURS_IN_A_DAY = 8;
         int PART_TIME_WORKING_HOURS = 8;
+        int WORKING_HOURS_IN_A_MONTH=20;
         // Variables
         int fullTimeattendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
         int partTimeAttendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
@@ -27,12 +28,14 @@ public class App {
             case 0:
                 System.out.println("Nature Of Employee: Full Time , Status: Present");
                 System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, WORKING_HOURS_IN_A_DAY);
-                System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY));
+                System.out.printf("Wages Earned in a Day=%d\n", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY));
+                System.out.printf("Wages Earned in a Month=%d\n", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY*WORKING_HOURS_IN_A_MONTH));
                 break;
             case 1:
                 System.out.println("Nature Of Employee: Part Time , Status: Present");
                 System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, PART_TIME_WORKING_HOURS);
-                System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS));
+                System.out.printf("Wages Earned in a Day=%d\n", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS));
+                System.out.printf("Wages Earned in a Month=%d", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS*WORKING_HOURS_IN_A_MONTH));
                 break;
             default:
                 System.out.println("Employee is Absent");
