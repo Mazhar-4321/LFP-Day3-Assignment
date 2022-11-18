743d7f797b55f6bb1622a9697e0e78ad912e6afc
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 06:38:29 +0530

    MODIFIED: Code to beautify statements

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,8 +10,8 @@ public class App {
         int attendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
         if (attendance == IS_FULL_TIME) {
             System.out.println("Employee is Present");
-            System.out.printf("Wage Per Hour=%d, Working Hours=%d\n",WAGE_PER_HOUR,WORKING_HOURS_IN_A_DAY);
-            System.out.printf("Wages Earned in a Day=%d",(WAGE_PER_HOUR*WORKING_HOURS_IN_A_DAY));
+            System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, WORKING_HOURS_IN_A_DAY);
+            System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY));
         } else {
             System.out.println("Employee is Absent");
         }
