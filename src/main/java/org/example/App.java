e4b76ef98ce1241a56a0a93517fd7fed8666073f
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 06:32:58 +0530

    ADDED: Code to calculate daily wages of an Employee

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,9 +5,13 @@ public class App {
         System.out.println("Hello World!");
         // Constants
         int IS_FULL_TIME = 1;
+        int WAGE_PER_HOUR = 20;
+        int WORKING_HOURS_IN_A_DAY = 8;
         int attendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
         if (attendance == IS_FULL_TIME) {
             System.out.println("Employee is Present");
+            System.out.printf("Wage Per Hour=%d, Working Hours=%d\n",WAGE_PER_HOUR,WORKING_HOURS_IN_A_DAY);
+            System.out.printf("Wages Earned in a Day=%d",(WAGE_PER_HOUR*WORKING_HOURS_IN_A_DAY));
         } else {
             System.out.println("Employee is Absent");
         }
