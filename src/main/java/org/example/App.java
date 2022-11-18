7c85fc2ba20ed1d42d8e260f775c35147d3e7a5f
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 11:00:07 +0530

    MODIFIED: Code to Change logic

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,27 @@
 package org.example;
 
 public class App {
+    static final int IS_FULL_TIME = 1;
+    static final int WAGE_PER_HOUR = 20;
+    static int FULL_TIME_WORKING_HOURS = 8;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // Constants
-        int IS_FULL_TIME = 1;
-        int WAGE_PER_HOUR = 20;
-        int WORKING_HOURS_IN_A_DAY = 8;
-        int attendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
-        if (attendance == IS_FULL_TIME) {
-            System.out.println("Employee is Present");
-            System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, WORKING_HOURS_IN_A_DAY);
-            System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY));
+        // Variables
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        int empWages = 0;
+        int workingHours = 0;
+        String empStatus = "Present";
+        // Functionality
+        if (empCheck == IS_FULL_TIME) {
+            empStatus = "Present";
+            empWages = FULL_TIME_WORKING_HOURS * WAGE_PER_HOUR;
+            workingHours = FULL_TIME_WORKING_HOURS;
         } else {
-            System.out.println("Employee is Absent");
+            empStatus = "Absent";
         }
+        System.out.println(empStatus);
+        System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, workingHours);
+        System.out.printf("Wages Earned in a Day=%d", empWages);
     }
 }
