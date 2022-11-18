a69f948a2e656be851054128549ed455670cf0ac
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 10:53:18 +0530

    MODIFIED: Code to remove unwanted elements

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,29 +1,36 @@
 package org.example;
 
 public class App {
+    static final int IS_FULL_TIME = 1;
+    static final int IS_PART_TIME = 2;
+    static final int WAGE_PER_HOUR = 20;
+    static int PART_TIME_WORKING_HOURS = 4;
+    static int FULL_TIME_WORKING_HOURS = 8;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // Constants
-        int IS_PRESENT = 1;
-        int WAGE_PER_HOUR = 20;
-        int WORKING_HOURS_IN_A_DAY = 8;
-        int PART_TIME_WORKING_HOURS = 8;
         // Variables
-        int fullTimeattendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
-        int partTimeAttendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        int empWages = 0;
+        int workingHours = 0;
+        String empStatus = "Present";
         // Functionality
-        if (fullTimeattendance == IS_PRESENT) {
-            System.out.println("Nature Of Employee: Full Time , Status: Present");
-            System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, WORKING_HOURS_IN_A_DAY);
-            System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY));
+        if (empCheck == IS_FULL_TIME) {
+            empStatus = "Full Time";
+            empWages = FULL_TIME_WORKING_HOURS * WAGE_PER_HOUR;
+            workingHours = FULL_TIME_WORKING_HOURS;
         } else {
-            if (partTimeAttendance == IS_PRESENT) {
-                System.out.println("Nature Of Employee: Part Time , Status: Present");
-                System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, PART_TIME_WORKING_HOURS);
-                System.out.printf("Wages Earned in a Day=%d", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS));
+            if (empCheck == IS_PART_TIME) {
+                empStatus = "Part Time";
+                empWages = PART_TIME_WORKING_HOURS * WAGE_PER_HOUR;
+                workingHours = PART_TIME_WORKING_HOURS;
             } else {
-                System.out.println("Employee is Absent");
+                empStatus = "Absent";
+
             }
         }
+        System.out.println(empStatus);
+        System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, workingHours);
+        System.out.printf("Wages Earned in a Day=%d", empWages);
     }
 }
