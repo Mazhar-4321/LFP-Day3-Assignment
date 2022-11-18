a8e52773128d2167bad475c052948e7e460c0f5b
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 08:14:41 +0530

    ADDED: Code to calculate Employee wages for a month or 100 hours

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,11 +8,14 @@ public class App {
         int WAGE_PER_HOUR = 20;
         int WORKING_HOURS_IN_A_DAY = 8;
         int PART_TIME_WORKING_HOURS = 8;
-        int WORKING_HOURS_IN_A_MONTH=20;
+        int WORKING_HOURS_IN_A_MONTH = 20;
         // Variables
         int fullTimeattendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
         int partTimeAttendance = (int) Math.floor(Math.random() * 10) % 2 + 1;
         int argumentVariable;
+        int totalWoringHours = 0;
+        boolean isActive = true;
+        int noOfDays = 0;
         if (fullTimeattendance == IS_PRESENT) {
             argumentVariable = 0;
         } else {
@@ -29,17 +32,24 @@ public class App {
                 System.out.println("Nature Of Employee: Full Time , Status: Present");
                 System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, WORKING_HOURS_IN_A_DAY);
                 System.out.printf("Wages Earned in a Day=%d\n", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY));
-                System.out.printf("Wages Earned in a Month=%d\n", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY*WORKING_HOURS_IN_A_MONTH));
+                System.out.printf("Wages Earned in a Month=%d\n", (WAGE_PER_HOUR * WORKING_HOURS_IN_A_DAY * WORKING_HOURS_IN_A_MONTH));
                 break;
             case 1:
                 System.out.println("Nature Of Employee: Part Time , Status: Present");
                 System.out.printf("Wage Per Hour=%d, Working Hours=%d\n", WAGE_PER_HOUR, PART_TIME_WORKING_HOURS);
                 System.out.printf("Wages Earned in a Day=%d\n", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS));
-                System.out.printf("Wages Earned in a Month=%d", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS*WORKING_HOURS_IN_A_MONTH));
+                System.out.printf("Wages Earned in a Month=%d", (WAGE_PER_HOUR * PART_TIME_WORKING_HOURS * WORKING_HOURS_IN_A_MONTH));
                 break;
             default:
                 System.out.println("Employee is Absent");
                 break;
+        }
+        while (isActive) {
+            totalWoringHours += (int) Math.floor(Math.random() * 10) % 8 + 1;
+            noOfDays += (int) Math.floor(Math.random() * 10) % 2 + 1;
+            if (totalWoringHours == 100 || noOfDays == 20) {
+                isActive = false;
+            }
         }
 
     }
